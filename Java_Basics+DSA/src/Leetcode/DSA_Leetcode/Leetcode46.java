c7a3d0752b001b076c11e912287adf484149024f
Apoorv <apoorvsharma0708@gmail.com>
2023-08-31 20:32:23 +0530

    Merge remote-tracking branch 'origin/master'

--- Java_Basics+DSA/src/Leetcode/DSA_Leetcode/Leetcode46.java
+++ Java_Basics+DSA/src/Leetcode/DSA_Leetcode/Leetcode46.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Leetcode46 {
+
     public static void main(String[] args) {
         int [] nums = {1,2,3};
         System.out.println(permute(nums));
